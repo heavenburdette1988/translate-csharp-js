0acfff505fe56abfdc740cdb93e4bc62608c94a4
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-20 17:19:34 -0500

    almost ther

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,33 @@
 // Put your code here
+
+
+
+
+
+
+
+for (let i = 0; i < 10; i++) 
+
+
+{
+let die0 = [{char: [""],
+value: [0]}]
+
+console.log(die0.char)
+// let die0[0].value = getRandomInt(1,6)
+const die1 = getRandomInt(1,6)
+const die2 = getRandomInt(1,6)
+let message  = `${die1} + ${die2} == ${die1.Value + die2.Value}`;
+
+    console.log(`The number is ${dieLoop[i]}`);
+  }
+
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
