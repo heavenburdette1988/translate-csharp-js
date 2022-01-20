0ac0207ca721648b5f3f7ca0223ccfe08bb8e6b7
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-20 15:24:49 -0500

    finished enemies

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,50 @@
 // Put your code here
+
+console.log("My Enemies List!")
+console.log("----------------")
+
+const enemies = 
+    [
+        {
+    FirstName: "Joshua",
+    LastName: "Flowers",
+        offenses:["Being a jerk to me in elementary school",
+        "Not being nice to me in elementary school"],
+        IsReallyHated: true
+    },
+    {
+        FirstName: "Darth",
+        LastName: "Vader",
+        offenses:["Cut off Luke's hand",
+        "Murdered all those kids",
+        "Unkind management practices"],
+        IsReallyHated: false
+    },
+
+    {
+        FirstName: "Betty",
+        LastName: "Rudelady",
+        offenses:[ "Phone calls in the theater",
+        "Phone calls on the bus",
+        "Phone calls in line at the grocery store",
+        "Poor conversationalist"],
+        IsReallyHated: true
+    },
+    {
+        FirstName: "Leon",
+        LastName: "Peck",
+        offenses:[ "Keeps giving me a hotplate"],
+        IsReallyHated: false  
+    }
+
+    ]
+
+    for (let enemy of enemies) {
+        // console.log(enemy.FirstName);
+
+        if(enemy.IsReallyHated === true){
+            console.log(enemy.FirstName, enemy.LastName, "is really really disliked")
+        
+      } else {
+       console.log(enemy.FirstName, enemy.LastName)   
+      }}
