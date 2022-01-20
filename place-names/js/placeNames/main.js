af59d697c5c3da20348cc077f1f86ca05cdfd76c
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-20 14:28:53 -0500

    place names to js complete

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,24 @@
 // Put your code here
+
+const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+    console.log("All Place Names")
+    
+for (let name of names) {
+  console.log(name);
+}
+
+console.log("")
+
+
+// const small = numbers.filter((n) => n < 10);
+
+const theNames = names.filter(n => n.startsWith("The"))
+
+console.log(theNames)
+
+for (let name of theNames) {
+    console.log(name);
+  }
