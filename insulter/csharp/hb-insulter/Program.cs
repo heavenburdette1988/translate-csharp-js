b20fd9a1701cf25f44656e04724cdfe9ba517e01
heavenburdette1988 <heavenburdette1988@gmail.com>
2022-01-21 15:01:58 -0500

    finished fizzbuzz

--- insulter/csharp/hb-insulter/Program.cs
+++ insulter/csharp/hb-insulter/Program.cs
@@ -6,18 +6,7 @@ namespace Insulter
     {
         static void Main (string[] args)
         {
-            
-            List <string> insults = new List<string> () 
-            {
-                "1, You look like what morning breath smells like.",
-                "2, If you tried to give me cpr I would probably throw myself back under water",
-                "3,I am not a fan of you",
-                "4, You think you're so special...and you are. The fact of your existence is nearly impossible to believe",
-                "5, I'd rather walk than be on the same plane as you",
-                "6, If I were given a million dollars to hang out with you...I would, but then I'd have to spend half of it on therapy",
-                "7, Do you have to be so...like that?"
-            };
-
+        
 
             List <int> indexes = new List<int> (){} ;
 //Javascrit .Length = C# .Count
